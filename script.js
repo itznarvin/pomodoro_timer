9630fe78b19e6e3ed3829c8d53bb23cb1a96fcd6
Narvin Parameswaran <narvincoding@gmail.com>
2025-08-11 17:40:32 +0800

    I have code the html and css file, working on the js file.

---
+++ script.js
@@ -0,0 +1,8 @@
+const minuteDisplay = document.getElementById('minutes')
+const secondsDisplay = document.getElementById('seconds')
+const startBtn = document.getElementById('start')
+const pauseBtn = document.getElementById('pause')
+const resetBtn = document.getElementById('reset')
+
+let timer;
+
